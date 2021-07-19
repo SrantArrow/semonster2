224274c50f6c30263c3f29241cd3d1402c4b0a32
ma2mo10 <yoshikikun318@gmail.com>
2021-07-19 10:30:17 +0900

    add Moster class

---
+++ app/src/main/java/semonster2/Monster.java
@@ -0,0 +1,23 @@
+package semonster2;
+
+public class Monster {
+  String name;
+  int rare;// 1:normal,2:uncommon,3:rare,4:ultra rare
+
+  Monster() {
+    Random random = new Random();
+    this.name = this.summonMonster(random.nextInt(5));
+    this.rare = random.nextInt(5);
+  }
+
+  String summonMonster(int mnumber) {
+    String monsters[] = { "マグマそうへい", "ヒサミツドラゴン", "スラリン", "デュラハン", "シーサーペント" };
+    return monsters[mnumber];
+  }
+
+  @Override
+  public String toString() {
+
+    return this.name + ":レア度[" + this.rare + "]";
+  }
+}
