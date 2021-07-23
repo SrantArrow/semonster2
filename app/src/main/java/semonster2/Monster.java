d99c7868a7c262e74b2e8f60160780a9f67951dd
SrantArrow <373.seiya@gmail.com>
2021-07-23 15:29:59 +0900

    add new monster

--- app/src/main/java/semonster2/Monster.java
+++ app/src/main/java/semonster2/Monster.java
@@ -3,15 +3,20 @@ import java.util.Random;
 
 public class Monster {
   String name;
-  int rare;// 1:normal,2:uncommon,3:rare,4:ultra rare
+  int rare;// 1:normal,2:uncommon,3:rare,4:ultra rare,5:ultimate rare
 
   Monster(int monstNum, int rareNum) {
     this.name = this.summonMonster(monstNum);
-    this.rare = rareNum;
+    if (this.name == "Blue Eyes OIT Dragon") {
+      this.rare = 5;
+    }
+    else {
+      this.rare = rareNum;
+    }
   }
 
   String summonMonster(int mnumber) {
-    String monsters[] = { "マグマそうへい", "ヒサミツドラゴン", "スラリン", "デュラハン", "シーサーペント" };
+    String monsters[] = { "マグマそうへい", "ヒサミツドラゴン", "スラリン", "デュラハン", "シーサーペント", "Blue Eyes OIT Dragon" };
     return monsters[mnumber];
   }
 
